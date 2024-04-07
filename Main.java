98ae5b98ac20b7584deb6e15403b56edf4f40580
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 11:52:39 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,12 @@ import geometry.ColoredCircle;
 public class Main {
     public static void main(String[] args) {
         Circle[] circles = new Circle[2];
-        circles[0] = new Circle(new Point(2.0, 1.5), 3.0);
-        circles[1] = new ColoredCircle(new Point(3.0, 4.0), 7.0, "red");
+        circles[0] = new Circle(new Point(5.0, 2.5), 6.0);
+        circles[1] = new ColoredCircle(new Point(4.0, 5.5), 8.0, "blue");
 
         for (Circle circle : circles) {
             double area = circle.calculateArea();
             System.out.println("Pole koła: " + area);
-
             if (circle instanceof ColoredCircle) {
                 String color = ((ColoredCircle) circle).getColor();
                 System.out.println("Kolor koła: " + color);
