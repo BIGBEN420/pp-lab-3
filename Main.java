777357b2a16a5e29c0717ad11272dfc155eebc82
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 11:53:39 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,8 @@ import geometry.Rectangle;
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(4.0, 3.0); 
-
         double area = rectangle.calculateArea();
         System.out.println("Pole prostokąta: " + area);
-
         double perimeter = rectangle.calculatePerimeter();
         System.out.println("Obwód prostokąta: " + perimeter);
     } 
