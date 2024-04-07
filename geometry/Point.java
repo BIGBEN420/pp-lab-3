a49dd59c230917b2e16f2fba32cb5d19a46aed37
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 11:51:25 +0200

    Update Point.java

--- geometry/Point.java
+++ geometry/Point.java
@@ -12,15 +12,12 @@ public class Point {
     public double getX() {
         return x;
     }
-
     public double getY() {
         return y;
     }
-
     public void setX(double x) {
         this.x = x;
     }
-
     public void setY(double y) {
         this.y = y;
     }
