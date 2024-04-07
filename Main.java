ab2c98a000b4735ec7a4d3581ed49c117e541641
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 11:29:00 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(6.0, 2.0);
+        System.out.println("Współrzędne:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    } 
+}
