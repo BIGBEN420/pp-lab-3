3aed85967698dd5fffd2fee63ef38e8f056bb915
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 12:01:08 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Point center = new Point(4.1, 8.3);
         ColoredCircle coloredCircle = new ColoredCircle(center, 6.2, "red");
-        System.out.println("właściwości koła kolorowego: ");
+        System.out.println("właściwości koła: ");
         System.out.println("Kolor koła: " + coloredCircle.getColor());
     } 
 }
