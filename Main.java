eb6c19e65efdd35b3870dd0e635676c821ef07fd
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-04-07 11:49:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Point center = new Point(8.0, 3.0);
         Circle circle = new Circle(center, 6.0);
-
         System.out.println("Wartości koła:");
         System.out.println("Promień koła: " + circle.getRadius());
         System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
